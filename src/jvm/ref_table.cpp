cd51960e2100c806201843b574a95869b2ea7c91
Adrian Freund <adrian@freund.io>
2020-08-13 13:08:51 +0200

    Fixed off-by-one in range check

--- src/jvm/ref_table.cpp
+++ src/jvm/ref_table.cpp
@@ -55,7 +55,7 @@ namespace FakeJni {
 
  void JniReferenceTable::deleteReference(size_t index) {
   index -= startIndex;
-  if (index < 0 || index > references.size())
+  if (index < 0 || index >= references.size())
    throw std::runtime_error("FATAL: Invalid out-of-bounds reference index");
   if (!references[index])
    throw std::runtime_error("FATAL: Invalid empty reference");
@@ -65,7 +65,7 @@ namespace FakeJni {
 
  std::shared_ptr<JObject> JniReferenceTable::getReference(size_t index) const {
   index -= startIndex;
-  if (index < 0 || index > references.size())
+  if (index < 0 || index >= references.size())
    return std::shared_ptr<JObject>();
   return references[index];
  }
